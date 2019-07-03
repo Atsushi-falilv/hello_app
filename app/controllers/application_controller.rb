f796de86133097b3a0942405695d9e362d71acaa
Takeuchi Atsushi <atsushi@servenet.co.jp>
2019-07-03 12:16:41 +0700

    Update Gemfile for Heroku atsushi

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello,Da nang!" 
+    render html: "hello,Da nang! atsushi" 
   end
 end
